7b8da76478eb242159d6451f215c61071bd2a3e9
HTTapadia <86509743+HTTapadia@users.noreply.github.com>
2023-11-26 11:36:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/VsGXDr4D/clown-nose.png');
 }
 
 function setup() {
@@ -18,14 +21,17 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-14;
+        noseY = results[0].pose.nose.y-14;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
-}
+}   
  
 function draw(){
     image(video, 0, 0, 300, 300);
-}
+    image(clown_nose, noseX, noseY, 30, 30);
+}   
 
 function take_Snapshot() {
     save('myFilterImage.png');
